108a3cfd82ee054ababfee1ee1155250513e3bfc
Prithvi Pal Singh <Prithvi.Coer@outlook.com>
2018-03-28 12:15:31 +0800

    Shuffling

--- Sorting/Program.cs
+++ Sorting/Program.cs
@@ -14,10 +14,10 @@ namespace Sorting
              * Area to call sorting algorithms
              * */
 
-            //1.Insertion sort
-            InsertionSort ins = new InsertionSort();
-            int[] items = Array.ConvertAll(Console.ReadLine().Split(' '), arrtemp => Convert.ToInt32(arrtemp));
-            ins.Sort(items);
+            ////1.Insertion sort
+            //InsertionSort ins = new InsertionSort();
+            //int[] items = Array.ConvertAll(Console.ReadLine().Split(' '), arrtemp => Convert.ToInt32(arrtemp));
+            //ins.Sort(items);
 
             //2. Insertion sort using IComparable
             //InsertionSort ins = new InsertionSort();
@@ -42,15 +42,20 @@ namespace Sorting
             //int[] arr = Array.ConvertAll(arr_temp, Int32.Parse);
             //insertionSort2(n, arr);
 
-            //5. Selection sort
-            SelectionSort ss = new SelectionSort();
-            int[] items1 = Array.ConvertAll(Console.ReadLine().Split(' '), arrtemp => Convert.ToInt32(arrtemp));
-            ss.Sort(items1);
+            ////5. Selection sort
+            //SelectionSort ss = new SelectionSort();
+            //int[] items1 = Array.ConvertAll(Console.ReadLine().Split(' '), arrtemp => Convert.ToInt32(arrtemp));
+            //ss.Sort(items1);
 
-            //6. H-Sorting
-            H_Sorting hs = new H_Sorting();
-            int[] items2 = Array.ConvertAll(Console.ReadLine().Split(' '), arrtemp => Convert.ToInt32(arrtemp));
-            hs.Sort(items2);
+            ////6. H-Sorting
+            //H_Sorting hs = new H_Sorting();
+            //int[] items2 = Array.ConvertAll(Console.ReadLine().Split(' '), arrtemp => Convert.ToInt32(arrtemp));
+            //hs.Sort(items2);
+
+            //7. Shuffle
+            Shuffling sf = new Shuffling();
+            int[] items3 = Array.ConvertAll(Console.ReadLine().Split(' '), arrtemp => Convert.ToInt32(arrtemp));
+            sf.shuffle(items3);
 
             Console.Read();
         }       
---
+++ Sorting/Shuffling.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+    class Shuffling
+    {
+        public void shuffle(int[] items)
+        {
+            int N = items.Length;
+
+            for (int i = 0; i < N; i++)
+            {
+                var r = new Random();
+                Exchange(items, r.Next(0, i + 1), i);
+            }
+
+            print(items);
+        }
+
+        private void print(int[] items)
+        {
+            int N = items.Length;
+
+            for (int i = 0; i < N; i++)
+            {
+                Console.Write(items[i] + " ");
+            }
+        }
+
+        private void Exchange(int[] arr, int minIndex, int indextoSwap)
+        {
+            var swap = arr[minIndex];
+            arr[minIndex] = arr[indextoSwap];
+            arr[indextoSwap] = swap;
+        }
+    }
+}
